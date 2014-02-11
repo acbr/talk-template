b2d7ac6dd296381fa844b269bc65de0f99c4dd11
Tiago Garcia <tiagoromerogarcia@gmail.com>
2014-02-11 00:16:16 -0200

    initial prototype

---
+++ Gruntfile.js
@@ -0,0 +1,147 @@
+module.exports = function(grunt) {
+  var port = grunt.option('port') || 8000;
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+        curly: false,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        eqnull: true,
+        browser: true,
+        expr: true,
+        globals: {
+          head: false,
+          module: false,
+          console: false,
+          unescape: false
+        }
+      },
+      files: ['Gruntfile.js', 'src/js/**/*.js']
+    },
+    csslint: {
+      options: {
+        'important': false,
+        'adjoining-classes': false,
+        'zero-units': false,
+        'unique-headings': false,
+        'qualified-headings': false,
+        'fallback-colors': false,
+        'universal-selector': false,
+        'compatible-vendor-prefixes': false,
+        'overqualified-elements': false,
+        'box-sizing': false,
+        'box-model': false,
+        'unqualified-attributes': false,
+        'import': false,
+        'display-property-grouping': false,
+        'known-properties': false,
+        'vendor-prefix': false,
+
+      },
+      src: ['src/css/**/*.css']
+    },
+    concat: {
+      js: {
+        src : ['src/js/reveal.js', 'src/js/**/*.js'],
+        dest : 'js/scripts.js'
+      },
+      css: {
+        src:  ['src/css/*.css', 'src/css/theme/*.css'],
+        dest: 'css/styles.css'
+      }
+    },
+    copy: {
+      css: {
+        files: [{
+          src: ['src/css/print/*'],
+          dest: 'css/print/',
+          flatten: true,
+          filter: 'isFile',
+          expand: true
+        }]
+      }
+    },
+    uglify: {
+      build: {
+        src: 'js/scripts.js',
+        dest: 'js/scripts.min.js'
+      }
+    },
+    cssmin: {
+      main: {
+        files: {
+          'css/styles.min.css': ['css/styles.css'],
+          'css/print/paper.min.css': ['css/print/paper.css'],
+          'css/print/pdf.min.css': ['css/print/pdf.css']
+        }
+      }
+    },
+    sass: {
+      main: {
+        files: {
+          'src/css/theme/avenuecode.css': 'css/theme/source/avenuecode.scss',
+        }
+      }
+    },
+    replace: {
+      options: {
+        patterns: [
+          {
+            json: grunt.file.readJSON('config.json')
+          }
+        ]
+      }
+    },
+    watch: {
+      main: {
+        files: ['src/**/*.js', 'src/**/*.css'],
+        tasks: ['jshint', 'csslint', 'concat', 'copy', 'uglify', 'cssmin']
+      },
+      theme: {
+        files: ['src/css/theme/**/*.scss'],
+        tasks: ['sass', 'csslint', 'concat:css', 'copy:css', 'cssmin']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: port,
+          base: '.'
+        }
+      }
+    },
+  });
+
+  // Dependencies
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-replace');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Default task
+  grunt.registerTask('default', ['jshint', 'csslint', 'concat', 'copy', 'uglify', 'cssmin', 'connect', 'watch']);
+
+  // Compile theme
+  grunt.registerTask('themes', ['sass']);
+
+  // Build task
+  grunt.registerTask('build', ['jshint', 'csslint', 'concat', 'copy', 'uglify', 'cssmin']);
+
+  // Serve presentation locally
+  grunt.registerTask('serve', ['connect', 'watch']);
+
+};
--- README.md
+++ README.md
@@ -2,3 +2,32 @@ Talk Template
 =============
 
 This is the standard Avenue Code template for Reveal.js slide decks.
+
+Instructions
+------------
+
+1. Install [Node.js](http://nodejs.org/)
+
+2. Install [Grunt](http://gruntjs.com/getting-started#installing-the-cli)
+
+4. Clone the reveal.js repository
+```sh
+$ git clone https://github.com/acbr/talk-template.git
+```
+
+5. Navigate to the reveal.js folder
+```sh
+$ cd talk-template
+```
+
+6. Install dependencies
+```sh
+$ npm install
+```
+
+7. Serve the presentation and monitor source files for changes
+```sh
+$ grunt serve
+```
+
+8. Open <http://localhost:8000> to view your presentation
